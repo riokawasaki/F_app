489ea689102fe0201397617dbd8c0690f5820189
Ubuntu <sukusukuhakusawa@icloud.com>
2025-09-29 03:31:10 +0000

    モデルの編集

--- app/models/fashion.rb
+++ app/models/fashion.rb
@@ -1,2 +1,6 @@
 class Fashion < ApplicationRecord
+    has_one_attached :image  # ActiveStorageで画像添付
+    validates :weather, presence: true
+    validates :temperature, presence: true
+    validates :recorded_on, presence: true
 end
